ecea9b2dd2038a6b2d1d236879ec76b0ea882660
Daniel Susetio <dsusetio87@gmail.com>
2023-11-29 14:35:10 +0100

    iterations 1-4

--- app.js
+++ app.js
@@ -22,4 +22,47 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI //  // Call the getBeers method from the Punk API wrapper
+  .getBeers()
+  .then(beersFromApi => {   // Handle the successful response
+    console.log("fetching beers");
+    res.render('beers', { beers: beersFromApi });  // Render the 'beers' view, passing the beers data to it
+
+  })
+  .catch(error => {   // Handle any errors that occur during the API call
+    console.log(error);
+    res.render('error', { error: 'Error fetching beers' });   // Render an 'error' view, passing an error message
+
+  });
+
+  console.log("fetching beers");
+
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getBeers()  // Fetch all beers
+    .then(beersFromApi => {
+      console.log("fetching a random beer");
+      if (beersFromApi.length > 0) {
+        const randomIndex = Math.floor(Math.random() * beersFromApi.length);  // Get a random index
+        const randomBeer = beersFromApi[randomIndex];  // Select a random beer
+        res.render('random-beer', { beer: randomBeer });  // Render the view with the random beer
+      } else {
+        res.render('error', { error: 'No beers available' });  // In case no beers are returned from the API
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error', { error: 'Error fetching beers' });  // Render an error view in case of an API call failure
+    });
+});
+  
+
+
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
